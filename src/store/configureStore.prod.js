9711f1c2f6deb9d49678f23c8c642b9ec4698af5
Anton Semenov <anton@anton.local>
2016-12-07 21:05:48 +0500

    add next question middleware

--- src/store/configureStore.prod.js
+++ src/store/configureStore.prod.js
@@ -1,11 +1,11 @@
 import {createStore, applyMiddleware} from 'redux'
 import rootReducer from '_reducer';
 import thunk from 'redux-thunk'
-import {toggleMiddleware} from './middleware'
+import {toggleMiddleware, nextQuestionMiddleware} from './middleware'
 import {loadGenerators} from '_actions'
 
 const configureStore = (toggleCallback) => {
-  const store = createStore(rootReducer, applyMiddleware(thunk,toggleMiddleware(toggleCallback)))
+  const store = createStore(rootReducer, applyMiddleware(thunk, toggleMiddleware(toggleCallback), nextQuestionMiddleware))
   store.dispatch(loadGenerators())
   return store
 }
--- src/store/middleware.js
+++ src/store/middleware.js
@@ -1,8 +1,27 @@
 import * as actionTypes from '_actions'
+import * as actions from '_actions'
+import {getQuestions, getCallback, getAnswers} from '_reducer'
 
 export const toggleMiddleware = toggleCallback => store => next => action => {
   if (action.type === actionTypes.TOGGLE) {
-    toogleCallback()
+    toggleCallback()
   }
+
   return next(action)
+}
+
+export const nextQuestionMiddleware = store => next => action => {
+  const result = next(action)
+
+  if (action.type === actionTypes.NEXT) {
+    const nextState = store.getState()
+    const questions = getQuestions(nextState)
+    if (questions.length === 0) {
+      getCallback(nextState)(getAnswers(nextState))
+    } else {
+      actions.loadNextQuestion(questions[0])
+    }
+  }
+
+  return result
 }
