014b400835352777ec245732ed212d479328866d
Anton Semenov <anton@anton.local>
2016-12-22 10:00:46 +0500

    Solved eval error

--- src/index.js
+++ src/index.js
@@ -2,15 +2,19 @@
 require('./l10n.min')
 import React from 'react'
 import ReactDOM from 'react-dom'
+import vm from 'vm'
 import Root from './components/root/Root'
 import configureStore from './store/configureStore'
 import {CompositeDisposable} from 'atom'
 import {loadGenerators} from '_actions'
 
+
 let element
 let subscriptions
 let modalPanel
 let dispatch
+let originalFunction
+let unsafeFunction
 
 export const activate = state => {
   const store = configureStore(toggle)
@@ -18,6 +22,22 @@ export const activate = state => {
   element.id = 'yeomanForAtomRoot'
   dispatch = store.dispatch
   ReactDOM.render(<Root store={store} />, element)
+
+  originalFunction = global.Function
+  unsafeFunction = (...args) => {
+    const params = []
+    const src = args[args.length - 1]
+    args.forEach((arg, index) => {
+      if (index === (args.length - 1)) return
+
+      if (typeof arg === 'string') {
+        const paramList = arg.split(/\s*,\s*/)
+        params.push(...paramList)
+      }
+    })
+
+    return vm.runInThisContext(`(function(${params.join(', ')}) {${src}})`)
+  }
 
   modalPanel = atom.workspace.addModalPanel({
     item: element,
@@ -43,8 +63,10 @@ export const serialaze = () => {}
 export const toggle = () => {
   if (modalPanel && modalPanel.isVisible()) {
     modalPanel.hide()
+    global.Function = originalFunction
   } else {
     modalPanel.show()
+    global.Function = unsafeFunction
     dispatch(loadGenerators())
   }
 }
